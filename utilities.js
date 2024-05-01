0b61851b8fc9ed714e6612156aeb29b8afe4d022
NigarShamil <shamilovanigar5@gmailcom>
2024-05-01 18:59:17 +0400

    adding another request

--- utilities.js
+++ utilities.js
@@ -5,13 +5,18 @@ function authenticateToken(req, res, next) {
     const authHeader = req.headers["authorization"];
     const token = authHeader && authHeader.split(" ")[1];
 
-    if (!token) return res.sendStatus(400);
+    if (!token) {
+        return res.status(401).json({ error: true, message: "Token is missing" });
+    }
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) return res.sendStatus(401);
+        if (err) {
+            console.error("JWT verification error:", err);
+            return res.status(401).json({ error: true, message: "Invalid token" });
+        }
         req.user = user;
         next();
-    })
+    });
 }
 
 module.exports = {
